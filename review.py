08cda430f4fa651e7e8ba5ce20c09e9289fd9ac2
Akshay Shekher <voldyman666@gmail.com>
2013-03-26 16:10:29 +0530

    only one loop over the lines in the file

--- review.py
+++ review.py
@@ -5,25 +5,21 @@ import sys
 def main():
     lines = open(sys.argv[1], "r").readlines()
 
-    # Check for single braces in a line
     for i in range(len(lines)):
+        # Check for single braces in a line
         if lines[i].strip() == "{":
             print "Suspicious line: %d" % (i + 1)
 
-    # Check for parentheses not preceded by a space
-    for i in range(len(lines)):
         for u in range(len(lines[i])):
+            # Check for parentheses not preceded by a space
             if lines[i][u] == "(" and lines[i][u - 1] != "(" and lines[i][u - 1] != " " and lines[i][u - 1] != "_":
                 print "Suspicious line: %d" % (i + 1)
 
-    # Check for equal signs that aren't spaced
-    for i in range(len(lines)):
-        for u in range(len(lines[i])):
+            # Check for equal signs that aren't spaced
             if lines[i][u] == "=" and lines[i][u - 1] != " " and lines[i][u + 1] != " " and lines[i][u + 1] != "\"":
                 print "Let's hope the non-spaced equal signs on line %d are inside parentheses." % (i + 1)
 
-    # Check for tabs
-    for i in range(len(lines)):
+        # Check for tabs
         if "\t" in lines[i]:
             print "Found a tab on line: %d" % (i + 1)
 
